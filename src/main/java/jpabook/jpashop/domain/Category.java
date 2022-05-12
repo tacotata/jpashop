32063602de4dd391e78923a3f783ed1d95774aea
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-12 21:28:16 +0900

    실전예제3- 다양한 연관관계 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id ;
+
+    private String name ;
+
+    @ManyToOne
+    @JoinColumn(name ="PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name ="CATEGORY_ITEM",
+                joinColumns = @JoinColumn(name ="CATEGORY_ID"),
+                inverseJoinColumns = @JoinColumn(name ="ITEM_ID"))
+    // 내가 조인하는건  joinColumns = @JoinColumn(name ="CATEGORY_ID"), 이거고
+    //반대편 테이블은 inverseJoinColumns = @JoinColumn(name ="ITEM_ID"))
+    private List<Item> items = new ArrayList<>();
+
+}
